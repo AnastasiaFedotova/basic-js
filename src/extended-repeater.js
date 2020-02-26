616e649771ab7d0863d510d2d1e6dd62144f6634
AnastasiaFedotova <anastaisafedotova@gmail.com>
2020-02-26 08:33:33 +0300

    feat: add solution task9

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,20 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+let stick = (string, stick, count) => {
+    if(string && count) {
+        string = (string + stick).repeat(count).split(stick);
+        string.pop()
+        return string.join(stick);
+    } return string;
+}
+module.exports = function repeater(str, options) {
+    let repeatTimes = options.repeatTimes || 0,
+        separator = options.separator || "+",
+        addition = String(options.addition) == "undefined" ? "" : String(options.addition),
+        additionRepeatTimes = options.additionRepeatTimes || 0,
+        additionSeparator = options.additionSeparator || "|";
+
+        addition = stick(addition, additionSeparator, additionRepeatTimes);
+        str = str + addition;
+        
+        return stick(str, separator, repeatTimes);
 };
   
