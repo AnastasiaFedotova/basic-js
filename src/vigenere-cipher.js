9a3d0cec05d052c6527d68d85e4355131c68007b
AnastasiaFedotova <anastaisafedotova@gmail.com>
2020-02-27 23:13:39 +0300

    feat: add solution task 10

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,58 @@
+
+
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(value) {
+        this.value = value === false ? value : true;
+        this.countLetters = 26;
+        this.charCodeA = 65;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if(!message || !key) throw new Error;
+        let result = "";
+        let count = 0;
+        for(let i = 0; i < message.length; i++) {
+            count >= key.length ? count = 0 : false;
+            let messageSymbol = message[i].toUpperCase();
+            let keySymbol = key[count].toUpperCase();
+            let rowIndex = messageSymbol.charCodeAt() - this.charCodeA;
+            let columnIndex = keySymbol.charCodeAt() - this.charCodeA;
+            if(rowIndex >= 0 && rowIndex <= this.countLetters) {
+                let resultSymbol =  String.fromCharCode(this.charCodeA + (rowIndex + columnIndex >= this.countLetters ? rowIndex + columnIndex - this.countLetters : rowIndex + columnIndex));
+                result += resultSymbol;
+                count++;
+            } else {
+                result += messageSymbol;
+           }
+        }
+        if(!this.value) {
+            return result.split("").reverse().join("");
+        }
+        return result;
+    }
+
+    decrypt(encryptedMessage, key) {
+        if(!encryptedMessage || !key) throw new Error;
+        let result = "";
+        let count = 0;
+        for(let i = 0; i < encryptedMessage.length; i++) {
+            count >= key.length ? count = 0 : false;
+            let messageSymbol = encryptedMessage[i].toUpperCase();
+            let keySymbol = key[count].toUpperCase();
+            let rowIndex = messageSymbol.charCodeAt() - this.charCodeA;
+            let columnIndex = keySymbol.charCodeAt() - this.charCodeA;
+            if(rowIndex >= 0 && rowIndex <= this.countLetters) {
+                let resultSymbol =  String.fromCharCode((rowIndex +  this.charCodeA - columnIndex) < this.charCodeA ? rowIndex +  this.charCodeA - columnIndex + this.countLetters: rowIndex +  this.charCodeA - columnIndex);
+                result += resultSymbol;
+                count++;
+            } else {
+                result += messageSymbol;
+           }
+        }
+        if(!this.value) {
+            return result.split("").reverse().join("");
+        }
+        return result;
     }
 }
 
