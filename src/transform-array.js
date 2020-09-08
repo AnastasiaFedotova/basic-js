ef6fbb78dc24edaf38d100dc6dd4dd1e6744d347
AnastasiaFedotova <anastaisafedotova@gmail.com>
2020-09-08 03:21:38 +0300

    fix: update transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,12 +3,17 @@ module.exports = function transform(arr) {
     let result = [];
 
     for(let i = 0; i < arr.length; i++) {
-        if (arr[i] == "--discard-next") i++;
-        else if (arr[i] == "--discard-prev") result.pop();
+        if (arr[i] == "--discard-next") {
+            arr[i + 1] = 'emptyElem'
+            i++;
+        }
+        else if (arr[i] == "--discard-prev") {
+            if(arr[i - 1] != 'emptyElem') result.pop()
+        }
         else if (arr[i] == "--double-next") {
             if (i < arr.length - 1) result.push(arr[i + 1]);
         } else if (arr[i] == "--double-prev") {
-            if (i > 0) result.push(arr[i - 1]);
+            if(arr[i - 1] != 'emptyElem' && i > 0) result.push(arr[i - 1]);
         } else result.push(arr[i]);
     }
 
