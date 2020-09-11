60aa22d1c20a1cd95780972ccc1ba3859107f2d5
AnastasiaFedotova <anastaisafedotova@gmail.com>
2020-09-11 16:22:09 +0300

    fix: add update tests, src transform-array/hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -2,5 +2,5 @@ module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     const ONE_HOUR_IN_SECONDS = 3600;
     let minTurns = Math.pow(2, disksNumber) - 1;
     let minSeconds = minTurns / (turnsSpeed / ONE_HOUR_IN_SECONDS);
-    return {turns: minTurns, seconds: minSeconds};
+    return {turns: minTurns, seconds: Math.floor(minSeconds)};
 }
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,21 +1,32 @@
 module.exports = function transform(arr) {
-    if(!Array.isArray(arr)) throw new Error;
     let result = [];
+    if(!Array.isArray(arr)) throw new Error;
+    if(arr.length == 1) return arr;
 
-    for(let i = 0; i < arr.length; i++) {
-        if (arr[i] == "--discard-next") {
-            arr[i + 1] = 'emptyElem'
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] == '--discard-prev' && arr[i - 2] != '--discard-next'){
+            result.pop();
+            continue;
+        }
+
+        else if (arr[i] == '--discard-next') {
+            i++;
+            continue;
+        }
+
+        else if(arr[i]==='--double-prev' && arr[i - 2] !== '--discard-next'){
+            if (arr[i-1] !== undefined) result.push(arr[i - 1]);
             i++;
         }
-        else if (arr[i] == "--discard-prev") {
-            if(arr[i - 1] != 'emptyElem') result.pop()
-        }
-        else if (arr[i] == "--double-next") {
-            if (i < arr.length - 1) result.push(arr[i + 1]);
-        } else if (arr[i] == "--double-prev") {
-            if(arr[i - 1] != 'emptyElem' && i > 0) result.push(arr[i - 1]);
-        } else result.push(arr[i]);
-    }
 
-   return result;
+        else if(arr[i] == '--double-next') {
+            if (arr[i+1] !== undefined) result.push(arr[i + 1]);
+            i++;
+        }
+
+        if (arr[i] != '--discard-next' && arr[i] != '--discard-prev' && arr[i] != '--double-next' && arr[i] != '--double-prev' && arr[i] !== undefined) result.push(arr[i]);
+
+    }
+    
+    return result;
 };
